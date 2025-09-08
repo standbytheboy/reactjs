fbfb6b9de180dc2e62cfbc505d4053143ecfebc3
standbytheboy <moraislcs05@gmail.com>
2025-09-08 10:25:47 -0300

    finalizando primeira parte aula 08/09

--- src/components/shopSidebar/ShopSidebar.jsx
+++ src/components/shopSidebar/ShopSidebar.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import './ShopSidebar.css';
 import Slider from 'rc-slider';
 
-const ShopSidebar = ({ listaDeCategorias }) => {
+const ShopSidebar = ({ listaDeCategorias, listaDeMarcas }) => {
     const [priceRange, setPriceRange] = useState([0, 500])
 
-
+    const aplicarFiltroPreco = () => {
+        console.log(`Filtro de preço aplicado: R\$ ${priceRange[0]} - R\$ ${priceRange[1]}`)
+    }
 
     return (
         <aside className='sidebar'>
@@ -15,7 +17,7 @@ const ShopSidebar = ({ listaDeCategorias }) => {
                     { listaDeCategorias.map((categoria) => {
                         <li key={categoria.nome} className='checkbox-item'>
                             <input type="checkbox" id={categoria} />
-                            <span> {categoria.nome} </span>
+                            <label> {categoria.nome} </label>
                             <span className='qtd-categoria'> {categoria.qtd} </span>
                         </li>
                     })}
@@ -31,6 +33,27 @@ const ShopSidebar = ({ listaDeCategorias }) => {
                     max={500}
                     defaultValue={priceRange}
                     onChange={(newRange) => (setPriceRange(newRange))} />
+
+                    <div className='price-info'>
+                        <span>
+                            Preço: R${priceRange[0]} - R${priceRange[1]}
+                        </span>
+
+                        <button onClick={aplicarFiltroPreco}>Aplicar</button>
+                    </div>
+                </div>
+
+                <div className='filter-group'>
+                    <h4>Filtrar por Marca</h4>
+                    <ul>
+                        {listaDeMarcas.map((marca) => (
+                            <li key={marca} className='checkbox-item'>
+                                <input type="checkbox" id={marca.nome}/>
+                                <label htmlFor={marca.nome}>{marca.nome}</label>
+                                <span className='qtd-categoria'>{marca.qtd}</span>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </aside>
